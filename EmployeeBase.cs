2b4b6a6e3e88d20ef96478e3c7ca938bfa2521c2
PAUL CHEN <pchenatwork@hotmail.com>
2019-06-05 15:24:45 -0400

    Add project files.

---
+++ EmployeeBase.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositePattern
+{
+    public abstract class EmployeeBase
+    {
+        protected string _name;
+        public EmployeeBase(string name)
+        {
+            _name = name;
+        }
+        /// <summary>
+        /// Unified Operation() among components
+        /// </summary>
+        public abstract void GetData();
+    }
+
+    public class Employee : EmployeeBase
+    {
+        public Employee(string Name) : base(Name) { }
+
+        public override void GetData()
+        {
+            Console.WriteLine(base._name);
+        }
+    }
+
+    public class Manager : EmployeeBase
+    {
+        private List<EmployeeBase> _teamMembers = new List<EmployeeBase>();
+
+        public Manager(string Name) : base(Name) { }
+        public void ManageMember(EmployeeBase employee)
+        {
+            _teamMembers.Add(employee);
+        }
+
+        public override void GetData()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Manager Name = " + base._name );
+
+            if (_teamMembers.Count>0)
+                sb.Append(", has " + _teamMembers.Count.ToString() + " teammate");
+
+            Console.WriteLine(sb.ToString());
+
+            foreach (var worker in _teamMembers)
+            {
+                worker.GetData();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee A1 = new Employee("Teammember A1");
+            A1.GetData();
+            Employee A2 = new Employee("Teammember A2");
+            A2.GetData();
+            Console.WriteLine("------------------------------");
+
+            Manager TeamLeadA = new Manager("Team A Lead");
+            TeamLeadA.ManageMember(A1);
+            TeamLeadA.ManageMember(A2);
+            TeamLeadA.GetData();
+            Console.WriteLine("------------------------------");
+
+            Employee B1 = new Employee("Developer B1");
+            Employee B2 = new Employee("Developer B2");
+            Employee B3 = new Employee("Developer B3");
+
+            Manager TeamLeadB= new Manager("Team B Lead");
+            TeamLeadB.ManageMember(B1);
+            TeamLeadB.ManageMember(B2);
+            TeamLeadB.ManageMember(B3);
+
+            Manager DepartmentHead = new Manager("Department Head");
+            Employee Secretary = new Employee("Department Secretary");
+
+            DepartmentHead.ManageMember(TeamLeadA);
+            DepartmentHead.ManageMember(TeamLeadB);
+            DepartmentHead.ManageMember(Secretary);
+
+            DepartmentHead.GetData();
+            Console.WriteLine("------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
